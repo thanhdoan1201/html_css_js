1d428094a82799d8cac4b397b24fcd7d8e36b3d9
Doan Nguyen <doannt1201@gmail.com>
2022-09-14 09:41:49 +0000

    Upload New File

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+var test=2;
+list = document.getElementsByClassName("hidden");
+num_list=list.length;
+
+//Run
+function toggle(a){
+if(test==2){
+    unset();
+    test=1}
+else if(test==1||a==''){
+    hidden();
+    test=2;
+}
+}
+//Fix nav hidden after resize window
+setInterval(function(){
+if(window.innerWidth>495){
+    unset();
+}},9^999)
+
+//Function set style
+function unset() {
+    for (index = 0; index < num_list; ++index) {
+        list[index].setAttribute("style", "visibility:unset");
+    };
+}
+function hidden(){
+    for (index = 0; index < num_list; ++index) {
+        list[index].setAttribute("style", "visibility:hidden");
+    };
+}
